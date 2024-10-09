85fd8ffa0b50499ca768a2e2d58ad1aa58517b80
KarthikRam14 <karthikram1441@gmail.com>
2024-10-09 11:46:31 +0530

    Created employees and customers section for the project

---
+++ Person.java
@@ -0,0 +1,54 @@
+public abstract class Person {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phoneNumber;
+
+    public Person(String firstName, String lastName, String email, String phoneNumber) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    // Abstract method - used by subclasses to get the type specifi to the subclass
+    public abstract String getType();
+
+    public void displayInfo() {
+        System.out.println("Name: " + firstName + " " + lastName);
+        System.out.println("Email: " + email);
+        System.out.println("Phone Number: " + phoneNumber);
+    }
+}
